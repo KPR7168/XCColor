e64356de22d2f602b2288ce42ff3c193d31f57df
KPR7168 <pich_ratanak@ymail.com>
2020-02-25 21:47:05 +1100

    Initial commit

--- XCColor/ViewController.swift
+++ XCColor/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "1234"
+        return cell
+    }
 
 }
 
