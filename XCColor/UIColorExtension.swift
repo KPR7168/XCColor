36c6a60fbed3bcdede22a10ac8f380212d30b7a5
KPR7168 <pich_ratanak@ymail.com>
2020-02-25 22:02:40 +1100

    add swiftgen

---
+++ XCColor/UIColorExtension.swift
@@ -0,0 +1,34 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+
+// MARK: - Color catalogs
+import UIKit
+
+public extension UIColor {
+
+  static let contentBlue = UIColor.color(named: "contentBlue")
+  static let contentGreen = UIColor.color(named: "contentGreen")
+  static let contentPink = UIColor.color(named: "contentPink")
+  static let contentYellow = UIColor.color(named: "contentYellow")
+  static let allColors: [UIColor] = [
+    contentBlue,
+    contentGreen,
+    contentPink,
+    contentYellow,
+  ]
+
+  static let allNames: [String] = [
+    "contentBlue",
+    "contentGreen",
+    "contentPink",
+    "contentYellow",
+  ]
+
+  private static func color(named: String) -> UIColor {
+      return UIColor(named: named)!
+  }
+}
+
+private final class BundleToken {}
+
